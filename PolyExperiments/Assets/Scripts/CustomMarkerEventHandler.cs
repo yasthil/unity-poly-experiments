b12abd66e2bbbe2e1ac676557832ab9b1ee459b9
Yasthil Bhagwandeen <deiago147@gmail.com>
2019-01-17 20:19:10 -0800

    Restructured code, implemented ModelLoader as a Singleton, only displaying models once the marker is detected.

---
+++ PolyExperiments/Assets/Scripts/CustomMarkerEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CustomMarkerEventHandler : DefaultTrackableEventHandler
+{
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        ModelLoader.Instance.LoadAssetsIntoContainer();
+    }
+
+}
---
+++ PolyExperiments/Assets/Scripts/ModelLoader.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PolyToolkit;
+
+public class ModelLoader : MonoBehaviour
+{
+    public int assetCount = 0;
+    public GameObject _parentContainer;
+    private static ModelLoader _instance;
+    private List<GameObject> _assetsLoaded;
+    private float _symbolYOffset = 0.5f;
+    private int _noOfAssetsToRuquest = 1;
+
+    public static ModelLoader Instance
+    {
+        get
+        {
+            return _instance;
+        }      
+    }
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    void Start()
+    {
+        _assetsLoaded = new List<GameObject>();
+
+        // Create a new request
+        PolyListAssetsRequest req = new PolyListAssetsRequest();
+        req.orderBy = PolyOrderBy.BEST;
+
+        // Search by keywords    
+        req.keywords = "dragon";
+
+        // Make the request with a callback function
+        PolyApi.ListAssets(req, GetSearchResults);
+    }
+
+    private void GetSearchResults(PolyStatusOr<PolyListAssetsResult> result)
+    {
+        // Set options for import so the assets aren't crazy sizes
+        PolyImportOptions options = PolyImportOptions.Default();
+        options.rescalingMode = PolyImportOptions.RescalingMode.FIT;
+        options.desiredSize = 1.0f;
+        options.recenter = true;
+
+        // List our assets
+        List<PolyAsset> assetsInUse = new List<PolyAsset>();
+
+        // Loop through the list and display the first x
+        for (int i = 0; i < Mathf.Min(_noOfAssetsToRuquest, result.Value.assets.Count); i++)
+        {
+            // Import our assets into the scene with the ImportDonuts function
+            PolyApi.Import(result.Value.assets[i], options, StoreResults);
+
+            assetsInUse.Add(result.Value.assets[i]);
+        }
+
+    }
+
+    /// <summary>
+    /// Stores the results.
+    /// </summary>
+    /// <param name="asset">Asset.</param>
+    /// <param name="result">Result.</param>
+    private void StoreResults(PolyAsset asset, PolyStatusOr<PolyImportResult> result)
+    {
+        assetCount++;
+        result.Value.gameObject.transform.position = new Vector3(0, _symbolYOffset, 0f);
+        result.Value.gameObject.SetActive(false);
+        _assetsLoaded.Add(result.Value.gameObject);
+    }
+
+
+    public void LoadAssetsIntoContainer()
+    {
+        GameObject iAsset;
+        for (int i = 0; i < _assetsLoaded.Count; i++)
+        {
+            iAsset = _assetsLoaded[i];
+            iAsset.SetActive(true);
+            iAsset.transform.SetParent(_parentContainer.transform);
+
+        }
+    }
+}
